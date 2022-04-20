e6e26151958a1f006234a13d2e0ee60779834313
Kyle Hippe <kyle.hippe@gmail.com>
2022-04-20 11:50:38 -0500

    Algorithm super class implemented. Need to test an implementation of this. Will try greedy search

--- ot2util/algorithm.py
+++ ot2util/algorithm.py
@@ -1,9 +1,9 @@
+import pathlib
 from pathlib2 import Path
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 
-from yaml import parse
 
-from ot2util.config import BaseSettings, parse_args
+from ot2util.config import BaseSettings, parse_args, PathLike
 from ot2util.experiment import Experiment, ExperimentManager
 
 '''
@@ -31,28 +31,55 @@ Design ideas
 
 '''
 
-class Algorithm(BaseSettings): 
+class Algorithm_Config(BaseSettings):
+    """Configuration for Algorithm super class. Contains data that should be present in any search algorithm 
+    for OT2. Any additional information will be ignored. 
+
+    TODO: 
+        - Figure out where to store the other information. 
+            * Should we just not validate and throw it into the config? Is that too confusing? 
+    """
+
+    class Config:  
+        extra = "ignore"
+
     # Remote directory path to stage experiments in
-    remote_dir: Optional[Path] = None
+    remote_dir: Optional[PathLike] = None
     # Remote host i.e. [user@]host[:port]
     host: Optional[str] = None
     # Private key path
-    key_filename: Optional[str] = None
+    key_filename: Optional[PathLike] = None
     # Path to opentrons_simulate or opentrons_execute directory
-    opentrons_path: Path = Path("/usr/bin")
+    opentrons_path: PathLike = Path("/usr/bin")
     # Whether or not to tar files before transferring from remote to local
     tar_transfer: bool = False
     # Directory to write experimental results to
-    output_dir: Path = ""
+    output_dir: PathLike = ""
     # Path to protocol script containing run function
-    protocol: Path = ""
+    protocol: PathLike = ""
     # Toggle simulation
     run_simulation: bool = True
 
 
-    def __repr__(self) -> str: 
-        return f"{type(self)}()"
+class Algorithm:
+    """Super class for user implemented search algorithms. Users should implement `__len__(), run()`
+    """
+    def __init__(self, config_path: PathLike) -> None:
+        """Initialize the algorithm superclass. Will only create the config model. 
+
+        Args:
+            config_path (PathLike): Path to YAML config file. 
+        """
+        self.config = Algorithm_Config.from_yaml(config_path)
     
+    def __repr__(self) -> str: 
+        return f"{type(self)}"
+
+    def __str__(self) -> str: 
+        
+        return f"{type(self)}({self.config.json()})"
+
+    #User should implement these    
     def __len__(self) -> int: 
         raise NotImplementedError 
 
@@ -61,8 +88,8 @@ class Algorithm(BaseSettings):
 
 
 def main(args):
-    abstract_algorithm = Algorithm.from_yaml(args)
-    print(repr(abstract_algorithm))
+    abstract_algorithm = Algorithm(args)
+    print(abstract_algorithm)
 
 if __name__ == "__main__":
     args = parse_args() 
