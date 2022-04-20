07dbc9c9d2750592ec2847456e23c2d9162d9055
Kyle Hippe <kyle.hippe@gmail.com>
2022-04-20 17:07:04 -0500

    Algorithm super class seems well defined. Now creates experiment manager

--- ot2util/algorithm.py
+++ ot2util/algorithm.py
@@ -1,34 +1,33 @@
-import pathlib
-from pathlib2 import Path
-from typing import Any, List, Optional, Union
+from pathlib import Path
+from typing import Any, Optional
 
 
 from ot2util.config import BaseSettings, parse_args, PathLike
-from ot2util.experiment import Experiment, ExperimentManager
+from ot2util.experiment import ExperimentManager
 
 '''
 Things to do: 
-    - have to figure out how to set the base algorithm class up with just some of the things 
-    found in the config file (extras=True???)
-        - can a model have an init? 
-        - can a model use methods 
-        - should we wrap the pydantic model into a more traditional class? 
-    - __str__() and __repr__() methods 
-    - figure out how to let run take as many arguments as you need 
     - proper order of making directories and what not during the experiment 
+        - remote_dir is confusing me, want to change to workdir, set working files regardless 
+            of working remotely or locally here. Or add a check to make sure we aren't using 
+            remote_dir when running locally (host == None?)
     - what happens if an experiment needs more setup than the base algorithm provides? 
-
+    - abstract experiments out further? a little bit of confusion is creaetd 
+        when creating the experiment in the run method 
+    - figure out how to handle individual protocol failure.
+        - store dependecies? If it is critical, cancel experiment, if not, continue without it? 
+    - what do we think about users defining their protocol config yaml. should 
+        we have an extensible way to do this? is there some universal parser that can take directions 
+        and produce an arbitrary protocol file? right now we are limited to running 
+        a specific number of aspirations/dispenses/well_moves/ so on. Probably not 
+        supposed to be in the algorithm class, but good to think about none the less. 
 
     
-
 Design ideas 
     - Do we do it like pytorch and have every 'model' setup its own forwards function
     (im thinking of calling this the run function)
         - what are the other options? not sure to be honest
     - is it still better to just have the algorithms live in the user space than to itegrate them? 
-    - also, I want to change 'remote_dir' to 'workdir' not sure if that causes namespace clashes though 
-
-
 '''
 
 class Algorithm_Config(BaseSettings):
@@ -36,15 +35,16 @@ class Algorithm_Config(BaseSettings):
     for OT2. Any additional information will be ignored. 
 
     TODO: 
-        - Figure out where to store the other information. 
-            * Should we just not validate and throw it into the config? Is that too confusing? 
+        - Think about storing information better. Currently just allowing 
+        extra fields to be added from the users config. Not the worst solution 
+        but potentially troublesome. 
     """
 
     class Config:  
-        extra = "ignore"
+        extra = "allow"
 
     # Remote directory path to stage experiments in
-    remote_dir: Optional[PathLike] = None
+    remote_dir: Optional[Path] = None
     # Remote host i.e. [user@]host[:port]
     host: Optional[str] = None
     # Private key path
@@ -54,7 +54,7 @@ class Algorithm_Config(BaseSettings):
     # Whether or not to tar files before transferring from remote to local
     tar_transfer: bool = False
     # Directory to write experimental results to
-    output_dir: PathLike = ""
+    output_dir: Path = Path("")
     # Path to protocol script containing run function
     protocol: PathLike = ""
     # Toggle simulation
@@ -70,7 +70,16 @@ class Algorithm:
         Args:
             config_path (PathLike): Path to YAML config file. 
         """
+        #create config object, continas info about experiments to run
         self.config = Algorithm_Config.from_yaml(config_path)
+        #create experiment manager 
+        self.experiment_manager = ExperimentManager(
+                            self.config.run_simulation,
+                            self.config.host,
+                            self.config.key_filename,
+                            self.config.opentrons_path,
+                            self.config.tar_transfer 
+        )
     
     def __repr__(self) -> str: 
         return f"{type(self)}"
