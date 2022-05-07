3c0a81fbe900514ad6c068e6eb09319027d4eb48
braceal <braceal@umich.edu>
2022-05-07 16:21:59 -0500

    handling errors first

--- ot2util/camera.py
+++ ot2util/camera.py
@@ -126,59 +126,57 @@ class Camera:
         img_markers = img.copy()
 
         # For now we use 4 markers to find the locations for plate 1
-        if len(corners) == 4:
-            c2 = corners[2][0]
-            c1 = corners[0][0]
-            c_side = corners[3][0]
-            c1 = c1.astype(int)
-            c2 = c2.astype(int)
-            c_side = c_side.astype(int)
+        if len(corners) != 4:
+            raise ValueError("No markers found")
 
-            diameter_x = (c_side[1] - c_side[2]) / 8
-            radius_x = diameter_x / 2
-            diameter_y = (c1[1] - c2[0]) / 12
-            radius_y = diameter_y / 2
+        c2 = corners[2][0]
+        c1 = corners[0][0]
+        c_side = corners[3][0]
+        c1 = c1.astype(int)
+        c2 = c2.astype(int)
+        c_side = c_side.astype(int)
 
-            origin = (c_side[2][0], c2[0][1]) + np.array([-1, -1])
-            cv2.circle(img_markers, origin, 1, (0, 255, 0), 1)
-            cv2.line(img_markers, c2[0], c1[1], 100, 2)
+        diameter_x = (c_side[1] - c_side[2]) / 8
+        radius_x = diameter_x / 2
+        diameter_y = (c1[1] - c2[0]) / 12
+        radius_y = diameter_y / 2
 
-            origin_br = (c_side[1][0], c1[1][1]) + np.array([4, -2])
-            cv2.circle(img_markers, origin_br, 1, (0, 255, 0), 1)
+        origin = (c_side[2][0], c2[0][1]) + np.array([-1, -1])
+        cv2.circle(img_markers, origin, 1, (0, 255, 0), 1)
+        cv2.line(img_markers, c2[0], c1[1], 100, 2)
 
-            center_origin = origin + (radius_x + radius_y) * 0.9
-            center_origin = center_origin.astype(int)
+        origin_br = (c_side[1][0], c1[1][1]) + np.array([4, -2])
+        cv2.circle(img_markers, origin_br, 1, (0, 255, 0), 1)
 
-            center_br = origin_br - (radius_x + radius_y) * 0.9
+        center_origin = origin + (radius_x + radius_y) * 0.9
+        center_origin = center_origin.astype(int)
 
-            for i in range(0, 12):
-                for j in range(0, 8):
-                    current = center_origin + i * diameter_y + j * diameter_x
-                    current = current.astype(int)
-                    from_bottom = (
-                        center_br - (11 - i) * diameter_y - (7 - j) * diameter_x
-                    )
-                    from_bottom = from_bottom.astype(int)
-                    cur_y = from_bottom[1] if i > 6 else current[1]
-                    cur_x = from_bottom[0] if j > 4 else current[0]
-                    cur = (cur_x, cur_y)
-                    cv2.circle(img_markers, cur, 1, (0, 255, 0), 1)
+        center_br = origin_br - (radius_x + radius_y) * 0.9
 
-            cv2.line(img_markers, c_side[1], c_side[2], 100, 2)
-            for cornerset in corners:
-                cornerset = cornerset[0].astype(int)
-                # draw the markers
-                tl = cornerset[0]
-                tr = cornerset[1]
-                bl = cornerset[3]
-                br = cornerset[2]
+        for i in range(0, 12):
+            for j in range(0, 8):
+                current = center_origin + i * diameter_y + j * diameter_x
+                current = current.astype(int)
+                from_bottom = center_br - (11 - i) * diameter_y - (7 - j) * diameter_x
+                from_bottom = from_bottom.astype(int)
+                cur_y = from_bottom[1] if i > 6 else current[1]
+                cur_x = from_bottom[0] if j > 4 else current[0]
+                cur = (cur_x, cur_y)
+                cv2.circle(img_markers, cur, 1, (0, 255, 0), 1)
 
-                cv2.line(img_markers, tl, tr, 255, 1)
-                cv2.line(img_markers, tr, br, 255, 1)
-                cv2.line(img_markers, br, bl, 255, 1)
-                cv2.line(img_markers, bl, tl, 255, 1)
-        else:
-            raise Exception("No markers found")
+        cv2.line(img_markers, c_side[1], c_side[2], 100, 2)
+        for cornerset in corners:
+            cornerset = cornerset[0].astype(int)
+            # draw the markers
+            tl = cornerset[0]
+            tr = cornerset[1]
+            bl = cornerset[3]
+            br = cornerset[2]
+
+            cv2.line(img_markers, tl, tr, 255, 1)
+            cv2.line(img_markers, tr, br, 255, 1)
+            cv2.line(img_markers, br, bl, 255, 1)
+            cv2.line(img_markers, bl, tl, 255, 1)
 
         return img_markers, center_br, center_origin, diameter_x, diameter_y
 
